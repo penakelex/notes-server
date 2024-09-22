2cf7d9b7d056711239101cf9b637bc54c667d2f4
penakelex <alexeykoninsky129@gmail.com>
2024-09-22 12:34:12 +0700

    Main function update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use tokio::net::TcpListener;
 
+use crate::error::Result;
+use crate::state::ApplicationState;
+
+mod context;
 mod error;
 mod model;
 mod web;
 mod settings;
+mod state;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()>{
+    let application_state = ApplicationState::new();
     
-    let settings = settings::Settings::new().unwrap();
-    
-    let listener = TcpListener::bind(settings.server.address())
+    let listener = 
+        TcpListener::bind(application_state.settings.server.address())
         .await.unwrap();
     
-    axum::serve(listener, web::routes()).await.unwrap();
+    axum::serve(listener, web::routes(application_state))
+        .await.unwrap();
+    
+    Ok(())
 }
